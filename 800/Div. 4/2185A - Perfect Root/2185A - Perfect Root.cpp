c992f5372f3b43402fcda2ac05c4d9bfc18d328f
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 14:52:27 +0600

    [2185A - Perfect Root | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ 800/Div. 4/2185A - Perfect Root/2185A - Perfect Root.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  int n;
+  cin>>n;
+
+  for(int i=1;i<=n;i++){
+    
+    cout<<i*i<<" ";
+  }
+  cout<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
