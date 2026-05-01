29f98717f230a48f37c2563f7f82bc239bc430da
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 14:58:40 +0600

    [1335A - Candies and Two Sisters | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ 800/Div. 3/1335A - Candies and Two Sisters/1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    while (n--)
+    {
+        int j;
+        cin>>j;
+        int count=0;
+        int a=0;
+        if (j%2!=0)
+        { a=j/2;
+        
+            /* code */
+        }
+        
+       
+        else{
+            a=(j/2)-1;
+            
+        }
+
+        cout<<a<<endl;
+
+            
+        }  
+             return 0;
+    }
+    
+    
+   
