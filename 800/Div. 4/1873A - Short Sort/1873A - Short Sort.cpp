0cba961c6b58a05fb747485fc6d118b8839ef9e7
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 14:50:20 +0600

    [1873A - Short Sort | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 4/1873A - Short Sort/1873A - Short Sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  string s;
+  cin >>s;
+
+  if(s[0]=='a' or s[1]=='b' or s[2]=='c') cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+  
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
