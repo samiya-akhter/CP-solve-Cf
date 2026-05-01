342258953c394537769ff2f7ff9e71cec9bc209c
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 15:05:59 +0600

    [1866A - Ambitious Kid | 800] Accepted | Time: 78ms | Memory: 100KB

---
+++ 800/Others/1866A - Ambitious Kid/1866A - Ambitious Kid.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  int n;
+  cin>>n;
+  vector<int> v(n);
+  for(int i=0;i<n;i++){
+    int x;
+    cin>>x;
+    if(x<0) x=-x;
+    v.at(i)=x;
+    
+  }
+  
+  sort(all(v));
+  int a=v.at(0);
+  
+  cout<<a<<endl;
+
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  //cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
