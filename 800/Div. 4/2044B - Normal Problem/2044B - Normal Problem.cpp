7cac4092ce1d5b08d0cf81815ae2bbc8ea39a1f6
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 14:48:57 +0600

    [2044B - Normal Problem | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ 800/Div. 4/2044B - Normal Problem/2044B - Normal Problem.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+
+    for (int i = 2; i *i<= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void solve() {
+
+  string s;
+  cin>>s;
+  reverse(all(s));
+
+  for(auto &a:s){
+    if(a=='q') a='p';
+    else if(a=='p') a='q';
+  }
+
+  cout<<s<<endl;
+
+  
+
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
