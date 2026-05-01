1b6c9f1690177f85007142ab8c5d1403111d8352
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 15:01:11 +0600

    [1985A - Creating Words | 800] Accepted | Time: 15ms | Memory: 0KB

---
+++ 800/Div. 4/1985A - Creating Words/1985A - Creating Words.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  string s1,s2;
+  cin>>s1>>s2;
+  char temp;
+  temp =s1[0];
+  s1[0]=s2[0];
+  s2[0]=temp;
+  cout<<s1<<" "<<s2<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
