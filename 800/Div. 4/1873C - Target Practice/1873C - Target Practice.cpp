71b337d52eff0bd103b3b6df8d7cf9edf03f2831
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 15:06:09 +0600

    [1873C - Target Practice | 800] Accepted | Time: 15ms | Memory: 0KB

---
+++ 800/Div. 4/1873C - Target Practice/1873C - Target Practice.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  
+  char arr2[10][10];
+
+  int arr1[10][10] = {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,2,2,2,2,2,2,2,2,1},
+    {1,2,3,3,3,3,3,3,2,1},
+    {1,2,3,4,4,4,4,3,2,1},
+    {1,2,3,4,5,5,4,3,2,1},
+    {1,2,3,4,5,5,4,3,2,1},
+    {1,2,3,4,4,4,4,3,2,1},
+    {1,2,3,3,3,3,3,3,2,1},
+    {1,2,2,2,2,2,2,2,2,1},
+    {1,1,1,1,1,1,1,1,1,1}};
+
+  int point=0;
+
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      cin>>arr2[i][j];
+      if(arr2[i][j]=='X'){
+        point+=arr1[i][j];
+      }
+    }
+  }
+
+cout<<point<<endl;
+
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
