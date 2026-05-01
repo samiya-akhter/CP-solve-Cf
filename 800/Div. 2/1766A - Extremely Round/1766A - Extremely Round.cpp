31caf383c2a50f23265833f6768257bad17bd517
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 14:59:29 +0600

    [1766A - Extremely Round | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ 800/Div. 2/1766A - Extremely Round/1766A - Extremely Round.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+
+  int n,x;
+  cin>>n;
+  int count=0;
+
+    string s = to_string(n);
+    x=s.size();
+
+    int a=s[0]-'0';
+    int m=9*(x-1)+a;
+
+    cout<<m<<endl;
+
+        
+       
+  
+
+
+  
+
+  
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
