4888e822e153524924dcc5edb8bcdba50c1c7aa2
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 15:01:01 +0600

    [1829A - Love Story | 800] Accepted | Time: 15ms | Memory: 0KB

---
+++ 800/Div. 4/1829A - Love Story/1829A - Love Story.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  string s="codeforces";
+  string s2;
+  cin>>s2;
+  int count=0;
+  for(int i=0;i<10;i++){
+    if(s[i]!=s2[i]) count++;
+  }
+  cout<<count<<endl;
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
