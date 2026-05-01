eea4d607d33ae9a5a08d17c422437d6c626e1793
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 14:49:30 +0600

    [2225A - A Number Between Two Others | unrated] Accepted | Time: 31ms | Memory: 0KB

---
+++ Unrated/Div. 2/2225A - A Number Between Two Others/2225A - A Number Between Two Others.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+
+    for (int i = 2; i *i<= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void solve() {
+
+  int x,y;
+  cin>>x>>y;
+
+  if((x*2)<y){
+    cout<<"YES"<<endl;
+  }
+
+  else cout<<"NO"<<endl;
+
+
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
