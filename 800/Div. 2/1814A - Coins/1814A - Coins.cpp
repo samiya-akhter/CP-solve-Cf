4a37f1a29264fc76429f85064fe6be2aed776ce4
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-01 15:02:13 +0600

    [1814A - Coins | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ 800/Div. 2/1814A - Coins/1814A - Coins.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+
+  int n,k;
+  cin>>n>>k;
+
+  
+
+  if(n%2==0||(n - k) % 2 == 0){
+    cout<<"YES"<<endl;
+
+  }
+  else
+  cout<<"NO\n";
+
+  
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
