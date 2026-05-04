5b9ed90042ddd2313d0c90a5434ccff91f95d7f8
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-04 23:34:09 +0600

    [2037A - Twice | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ 800/Div. 3/2037A - Twice/2037A - Twice.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define no "No"
+#define yes "Yes"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
+
+bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
+
+void solve() {
+
+  int n;
+  cin>>n;
+
+
+
+  map<int ,int > freq;
+  int a;
+  for(int i=0;i<n;i++){
+    cin>>a;
+    freq[a]++;
+
+  }
+  int count=0;
+
+  for(auto a:freq){
+   count += a.second / 2;
+  }
+
+  cout<<count<<endl;
+
+
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
