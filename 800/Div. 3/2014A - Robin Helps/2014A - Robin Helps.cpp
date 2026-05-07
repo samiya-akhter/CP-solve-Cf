0e15f40763ff0ec4c27b53d23047a8a266ae5d81
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-07 22:42:32 +0600

    [2014A - Robin Helps | 800] Accepted | Time: 93ms | Memory: 0KB

---
+++ 800/Div. 3/2014A - Robin Helps/2014A - Robin Helps.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define no "No"
+#define yes "Yes"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
+
+bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
+
+void solve() {
+
+  int n,k;
+  cin>>n>>k;
+  int v[n];
+  int count=0;
+  int thr=0;
+  for(auto &a:v){
+    cin>>a;
+
+    if(a>=k){
+      thr+=a; 
+    }
+
+     if(a==0){
+      if(thr!=0){
+      thr--; 
+      count++;
+      }
+    }
+  }
+  cout<<count<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
