9d5da5d342b58ab71aadb51b7e0eb02cc5195ed0
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-24 22:44:21 +0600

    [2050A - Line Breaks | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 3/2050A - Line Breaks/2050A - Line Breaks.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define no "No"
+#define yes "Yes"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
+
+bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
+
+void solve() {
+
+  int n,m;
+  cin>>n>>m;
+  vector<string> v(n);
+  for(auto &a:v) cin>>a;
+  int count=0;
+  int p=0;
+
+  for(auto a:v){
+    p+=a.size();
+    if(p<=m) count++;
+    else break;
+
+  }
+
+  cout<<count<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
