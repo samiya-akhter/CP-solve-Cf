42291cf58d8124cc4c1c862c707f53a2c7bae61e
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-26 01:25:58 +0600

    [1679A - AvtoBus | 900] Accepted | Time: 31ms | Memory: 0KB

---
+++ 900/Div. 2/1679A - AvtoBus/1679A - AvtoBus.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define no "No"
+#define yes "Yes"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
+
+bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
+
+void solve() {
+
+  int n;
+  cin>>n;
+  int a,b;
+  if(n<4||n&1){
+    cout<<-1<<endl;
+    return;
+
+  }
+
+  a=n/6;
+
+  if(n%6)a++;
+  
+  b=n/4;
+ 
+
+
+  cout<<a<<" "<<b<<endl;
+
+
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
