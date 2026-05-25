79d7e0de07dad5ff36616d96e3446aece9d310eb
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-05-26 02:49:48 +0600

    [1606A - AB Balance | 900] Accepted | Time: 31ms | Memory: 0KB

---
+++ 900/Div. 2/1606A - AB Balance/1606A - AB Balance.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define no "No"
+#define yes "Yes"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
+
+bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
+
+void solve() {
+  string s;
+  cin >>s;
+  s[0]=s[s.size()-1];
+
+  cout<<s<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
