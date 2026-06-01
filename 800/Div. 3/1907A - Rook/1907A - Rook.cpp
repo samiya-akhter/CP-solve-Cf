efea014bf03609b914b20d4a61bef8e212440e2e
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-06-02 00:45:41 +0600

    [1907A - Rook | 800] Accepted | Time: 31ms | Memory: 0KB

--- 800/Div. 3/1907A - Rook/1907A - Rook.cpp
+++ 800/Div. 3/1907A - Rook/1907A - Rook.cpp
@@ -1,52 +1,52 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define int long long
 #define endl "\n"
 #define no "No"
 #define yes "Yes"
- 
+
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
- 
+
 const int INF = 1e18; // Large value for "infinity" 
- 
+
 int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
 int lcm(int a, int b){ return a / gcd(a,b) * b; }
- 
+
 bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
- 
+
 void solve() {
   string s;
   cin >>s;
- 
+
   for(int i = 1; i <= 8; i++){
         if(i == (s[1] - '0')) continue;
         cout << s[0] << i << endl;
   }
   
- 
+
  char a = 'a';
     for(int i = 0; i < 8; i++){
         if(a + i == s[0]) continue;
         cout << char(a + i) << s[1] << endl;
     }
- 
- 
- 
+
+
+
 }
- 
+
 int32_t main() {
- 
+
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t = 1;
   cin >> t;
- 
+
   while (t--) {
     solve();
   }
- 
+
   return 0;
 }
