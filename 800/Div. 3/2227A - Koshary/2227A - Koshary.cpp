fddfba090f6286d908254d4a0df2381aaccbb3e0
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-06-02 00:47:14 +0600

    [2227A - Koshary | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ 800/Div. 3/2227A - Koshary/2227A - Koshary.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define no "No"
+#define yes "Yes"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
+
+bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
+
+void solve() {
+  int a,b;
+  cin>>a>>b;
+  if((a&1) && (b&1)) cout << no << endl;
+else cout << yes << endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
